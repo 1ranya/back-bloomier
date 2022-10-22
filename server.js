776d4ee32d6ff68d79883c93513ade2db398b23f
1ranya <ranyamttn@gmail.com>
2022-10-22 14:04:29 +0200

    🚚 move script to the root

---
+++ server.js
@@ -0,0 +1,33 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const db = require('./src/models')
+
+const app = express();
+
+var corsOptions = {
+    origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Restart database
+// db.sequelize.sync({force: true}).then(()=>{
+//     console.log("Drop and resync db.")
+// });
+
+app.get('/', (req, res) => {
+    res.json({
+        response: "Hello Bloomier world"
+    });
+})
+
+require("./src/routes/photo.routes")(app);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Bloomier Server is running in the port ${PORT}`);
+})
