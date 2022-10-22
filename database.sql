b83d9e30c3bd4142cf110df893ee9b21657ecba2
1ranya <ranyamttn@gmail.com>
2022-10-22 11:02:00 +0200

    🗃️ [ database ] add database sql

---
+++ database.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE bloomier-db; 
+--\c bloomier-db
+
+CREATE TABLE IF NOT EXISTS photos(
+    id serial PRIMARY KEY, 
+    description VARCHAR ( 500 ), 
+    publisher VARCHAR ( 50 ), 
+    likes NUMERIC, category VARCHAR ( 50 ), 
+    createdAt TIMESTAMP NOT NULL, 
+    updatedAt TIMESTAMP NOT NULL
+);
+
+-- INSERT INTO photos(description, url, publisher, likes, category) VALUES ( "test", " ", "Rania", 1, "photography");
