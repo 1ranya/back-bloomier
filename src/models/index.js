07f1791c778caa145fc462834ea7a9a30951b486
1ranya <ranyamttn@gmail.com>
2022-10-26 10:18:55 +0200

    🗃️ handle createdAt and updatedAt issue

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,17 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, 
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, dbConfig.DB_URL,
     {
         host: dbConfig.HOST,
         dialect: dbConfig.dialect,
-
         pool: {
             max: dbConfig.pool.max,
             min: dbConfig.pool.min,
             acquire: dbConfig.pool.acquire,
             idle: dbConfig.pool.idle
-        }    
+        },   
+        define: dbConfig.define,
     }
 )
 
