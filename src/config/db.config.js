9ccef000f634d6b871d9d0c5377fc6f950dd8cd7
Ranya <35894795+1ranya@users.noreply.github.com>
2022-10-30 22:35:28 +0100

    Update db.config.js

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -9,14 +9,5 @@ module.exports = {
     DB: process.env.DB,
     dialect: process.env.DIALECT,
     DB_URL: process.env.DB_URL,
-    acquireConnectionTimeout: 5000,
-    pool: {
-        min: 0,
-        max: 10,
-        createTimeoutMillis: 8000,
-        acquireTimeoutMillis: 8000,
-        idleTimeoutMillis: 8000,
-        reapIntervalMillis: 1000,
-        createRetryIntervalMillis: 100,
-    }
+    acquireConnectionTimeout: 5000
   };
