3f82490239898a0c879da62e0f6386a381baddb6
Ranya <35894795+1ranya@users.noreply.github.com>
2022-10-31 08:53:52 +0100

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,29 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,
-    {
-        host: dbConfig.HOST,
-        dialect: "postgres",
-        "define": {
-            timestamps: false
-        },
-        port: 5432,
-        idleTimeoutMillis: 0,
-        connectionTimeoutMillis: 0,
-    }
-)
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD,
+//     {
+//         host: dbConfig.HOST,
+//         dialect: "postgres",
+//         "define": {
+//             timestamps: false
+//         },
+//         port: 5432,
+//         idleTimeoutMillis: 0,
+//         connectionTimeoutMillis: 0,
+//     }
+// )
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: 'dpg-cd9resarrk09hiokut1g-a',
+  username: 'admin',
+  password: 'eho1Sq9drePuYae5QZBdzKat1FZs3FKr',
+  database: 'bloomier_db',
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
 
 const db = {};
 
